68c3e778c8ae5d54fa311d80368d7f9b4f165fac
Liviu Calita <liviu.calita98@gmail.com>
2019-12-21 18:08:43 +0100

    le go

---
+++ Personale-Ledger/src/main/java/it/jac/java/it/jac/java/PersonaleLedgerApplication.java
@@ -0,0 +1,13 @@
+package it.jac.java.it.jac.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaleLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaleLedgerApplication.class, args);
+	}
+
+}
