cdea8bb0f28b9d6c1e7406a34c91e2e1ed55cf55
Liviu Calita <liviu.calita98@gmail.com>
2019-12-23 19:10:50 +0100

    move and fix servlet

--- Personale-Ledger/src/main/java/mainapp/PersonaleLedgerApplication.java
+++ Personale-Ledger/src/main/java/mainapp/PersonaleLedgerApplication.java
@@ -2,8 +2,9 @@ package mainapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
+@ServletComponentScan
 public class PersonaleLedgerApplication {
 
 	public static void main(String[] args) {
--- Personale-Ledger/src/main/java/mainapp/servlet/loginServlet.java
+++ Personale-Ledger/src/main/java/mainapp/servlet/loginServlet.java
@@ -1,4 +1,4 @@
-package form;
+package mainapp.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
