8076a8337cef21af119a0b13925b5ce7fc6d478b
Liviu Calita <liviu.calita98@gmail.com>
2019-12-23 17:43:33 +0100

    services

--- Personale-Ledger/src/main/java/services/TransactionServiceImpl.java
+++ Personale-Ledger/src/main/java/services/TransactionServiceImpl.java
@@ -1,5 +1,41 @@
 package services;
 
-public class TransactionServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import entities.Transaction;
+import repository.TransactionRepo;
+
+public class TransactionServiceImpl implements TransactionService{
+	@Autowired
+    public TransactionRepo transRepo;
+
+    public TransactionServiceImpl(TransactionRepo transRepo) {
+        this.transRepo = transRepo;
+    }
+	@Override
+	public List<Transaction> SelTutti() {
+		return this.transRepo.findAll();
+	}
+
+	@Override
+	public Transaction SelById(String id) {
+		// TODO Auto-generated method stub
+		return this.transRepo.findById(id);
+	}
+
+	@Override
+	public void InsUser(Transaction transaction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void DelUser(Transaction transaction) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
